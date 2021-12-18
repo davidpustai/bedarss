30a53ecb348745e36092bb5ba2d72d23276d12c7
David Pustai <david@pustai.cz>
2021-12-18 14:18:03 +0100

    Use double quotes

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
-require 'sinatra'
+require "sinatra"
 require "sinatra/reloader" if development?
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
 
 configure do
   disable :static
 end
 
 class BedarssApp < Sinatra::Base
-  get '/' do
-    document = Nokogiri::HTML.parse(open('http://lpu.cz/beda/'))
-    document.xpath('//comment()').remove
-    document.css('br').remove
+  get "/" do
+    document = Nokogiri::HTML.parse(open("http://lpu.cz/beda/"))
+    document.xpath("//comment()").remove
+    document.css("br").remove
     @items = document.css("li")
 
     headers "Content-Type" => "application/xml"
